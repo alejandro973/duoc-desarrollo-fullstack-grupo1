5a821d85774507ca918202f5801e9f89a1419fc0
alejandro973 <os.carrizo@duocuc.cl>
2026-06-16 17:27:09 -0400

    docs(Contratos): implementar y configurar documentacion de la API con Swagger UI

--- contratos/src/main/java/com/gym/contratos/controller/ContratoController.java
+++ contratos/src/main/java/com/gym/contratos/controller/ContratoController.java
@@ -9,23 +9,29 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/contratos")
 @RequiredArgsConstructor
+@Tag(name = "Contrato",description = "Operaciones relacionadas al contrato")
 public class ContratoController {
 
     private final ContratoService contratoService;
 
     
     @GetMapping
+    @Operation(summary = "Listar contratos",description = "Listar todos los contratos existentes")
     public ResponseEntity<List<ContratoResponseDto>> listarTodos() {
         return ResponseEntity.ok(contratoService.obtenerTodos());
     }
 
     
     @GetMapping("/{id}")
+    @Operation(summary = "Buscar Contratos",description = "Permite buscar contratos por id")
     public ResponseEntity<ContratoResponseDto> buscarPorId(@PathVariable Long id) {
         return contratoService.obtenerPorId(id)
                 .map(ResponseEntity::ok)
@@ -33,6 +39,7 @@ public class ContratoController {
     }
 
     @PostMapping
+    @Operation(summary = "Crear Contrato",description = "Permite crear contratos nuevos")
     public ResponseEntity<ContratoResponseDto> crear(@Valid @RequestBody ContratoRequestDto dto) {
         try {
             ContratoResponseDto nuevo = contratoService.guardar(dto);
