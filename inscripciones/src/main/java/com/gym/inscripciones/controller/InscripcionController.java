2d59764e1fec35df96c4c7798697ae195a69d6cc
alejandro973 <os.carrizo@duocuc.cl>
2026-06-16 14:28:00 -0400

    docs(Inscripciones): implementar y configurar documentacion de la API con Swagger UI

--- inscripciones/src/main/java/com/gym/inscripciones/controller/InscripcionController.java
+++ inscripciones/src/main/java/com/gym/inscripciones/controller/InscripcionController.java
@@ -9,22 +9,28 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/inscripciones")
 @RequiredArgsConstructor
+@Tag(name = "Inscripciones",description = "Operaciones relacionadas a las inscripciones del gym")
 public class InscripcionController {
 
     private final InscripcionService inscripcionService;
 
     @GetMapping
+    @Operation(summary = "Listar Inscripciones",description = "Permite listar todas las inscripciones del gym")
     public ResponseEntity<List<InscripcionResponseDto>> listarInscripciones() {
         return ResponseEntity.ok(inscripcionService.obtenerTodas());
     }
 
    
     @PostMapping
+    @Operation(summary = "Crear Inscripcion",description = "Permite la creacion de una nueva inscripcion")
     public ResponseEntity<?> realizarInscripcion(@Valid @RequestBody InscripcionRequestDto request) {
         try {
             InscripcionResponseDto nueva = inscripcionService.crearInscripcion(request);
@@ -37,6 +43,7 @@ public class InscripcionController {
 
    
     @DeleteMapping("/{id}")
+    @Operation(summary = "Cancelar Inscripcion",description = "Permite Cancelar la inscripcion por medio de su id")
     public ResponseEntity<Void> cancelar(@PathVariable Long id) {
         try {
             inscripcionService.cancelarInscripcion(id);
