e486e56a192f2a03216016174f76a16d53604f44
alejandro973 <os.carrizo@duocuc.cl>
2026-06-16 16:23:15 -0400

    docs(Sucursales): implementar y configurar documentacion de la API con Swagger UI

--- sucursales/src/main/java/com/gym/sucursales/controller/SucursalController.java
+++ sucursales/src/main/java/com/gym/sucursales/controller/SucursalController.java
@@ -9,23 +9,29 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/sucursales")
 @RequiredArgsConstructor
+@Tag(name = "Sucursal",description = "Operaciones relacionadas a las distintas sucursales")
 public class SucursalController {
 
     private final SucursalService sucursalService;
 
     // 1. Obtener todas las sucursales con sus equipos
     @GetMapping
+    @Operation(summary = "Listar sucursales",description = "Listar todas las sucursales existentes")
     public ResponseEntity<List<SucursalResponseDto>> listarTodas() {
         return ResponseEntity.ok(sucursalService.obtenerTodas());
     }
 
     // 2. Obtener una sucursal específica por ID
     @GetMapping("/{id}")
+    @Operation(summary = "Obtener por id",description = "Permite obtener una sucursal por su id")
     public ResponseEntity<SucursalResponseDto> obtenerPorId(@PathVariable Long id) {
         return sucursalService.obtenerPorId(id)
                 .map(ResponseEntity::ok)
@@ -34,6 +40,7 @@ public class SucursalController {
 
     // 3. Crear una nueva sucursal
     @PostMapping
+    @Operation(summary = "Crear Sucursal",description = "Permite crear una nueva sucursal")
     public ResponseEntity<SucursalResponseDto> crear(@Valid @RequestBody SucursalRequestDto dto) {
         SucursalResponseDto nueva = sucursalService.guardar(dto);
         return new ResponseEntity<>(nueva, HttpStatus.CREATED);
